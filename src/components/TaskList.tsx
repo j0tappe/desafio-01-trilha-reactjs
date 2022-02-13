0818c9a60e41edd4bc06148ac959536c89baf2da
Jotappe <jotappe-@hotmail.com>
2022-02-13 21:15:13 +0000

    handle task completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,33 +17,28 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
 
-    if (!newTaskTitle) {
-      alert('Insira um "to do"')
-      return;
-    } else {
+    if (!newTaskTitle) return;
 
-      const newTask = {
-        id: Math.random(),
-        title: newTaskTitle,
-        isComplete: false,
-      }
 
-      setTasks(tasks => [...tasks, newTask]); +
-        setNewTaskTitle('');
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
     }
+
+    setTasks(tasks => [...tasks, newTask]);
+    setNewTaskTitle('');
+
 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const checkedTask = tasks.map(task => task.id == id ? {
-      ...task,
-      //isComplete: true
-      iscomplete: !task.isComplete
-    } : task);
+    const checkedTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task)
 
     setTasks(checkedTask)
-
   }
 
   function handleRemoveTask(id: number) {
